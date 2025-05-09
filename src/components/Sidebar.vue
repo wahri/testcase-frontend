c282fb95b58c886535f492f3d8b81bc2b2135cc3
wahri <wahyubahri1212@gmail.com>
2025-05-09 22:06:12 +0700

    fix header

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -5,7 +5,7 @@
                 <img src="/assets/images/logo-icon.png" class="logo-icon" alt="logo icon" />
             </div>
             <div>
-                <h4 class="logo-text">ELSOFT</h4>
+                <h4 class="logo-text">TESTCASE</h4>
             </div>
             <div class="toggle-icon ms-auto"><i class="bx bx-first-page"></i></div>
         </div>
